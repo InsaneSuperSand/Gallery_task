2a703da670141d6b53a0070c4735a48c013641e1
InsaneSuperSand <necronomicon93@gmail.com>
2021-11-24 23:03:12 +0100

    cosmetics
    author in photo after hover - goal

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ const gallery = document.querySelector(".gallery");
 let cardTag;
 function getPhotos(images) {
   images.map((image) => {
-    cardTag = `<div class="gallery-pad">
-              <img src=${image.src.landscape}/>
-              <p> ${image.photographer}| ${image.title} </p>
-         </div>`;
+    cardTag = `
+                <div class="gallery-pad">
+                 <p class="author"> ${image.photographer} </p>
+                  <img class="gallery-image" src=${image.src.portrait}/>
+              
+                </div>
+              `;
     gallery.innerHTML += cardTag;
   });
 }
-fetch("https://api.pexels.com/v1/search?query=cyberpunk&per_page=9", {
-  headers: {
-    Authorization: "563492ad6f91700001000001cf063f04a3ee4a43ba2bd2ba1fc03aa1",
-  },
-})
+fetch(
+  "https://api.pexels.com/v1/search?query=cyberpunk&per_page=18&orientation=landscape&size=small",
+  {
+    headers: {
+      Authorization: "563492ad6f91700001000001cf063f04a3ee4a43ba2bd2ba1fc03aa1",
+    },
+  }
+)
   .then((resp) => {
     return resp.json();
   })
