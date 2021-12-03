ac9972b9401abf3b09ae959d29ce24a94bb2bee8
InsaneSuperSand <necronomicon93@gmail.com>
2021-12-03 19:49:18 +0100

    cleaning code
    code minimisation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getPhotos(images) {
   images.map((image) => {
     cardTag = `
                 <div class="gallery-pad">
-                  <img class="gallery-image" src=${image.src.portrait}/>
+                  <img class="gallery-image" src=${image.src.portrait} alt=${image.photographer}/>
                   <div class="info-banner">
                     <p class="author"> ${image.photographer} </p>
                   </div>
@@ -13,8 +13,9 @@ function getPhotos(images) {
     gallery.innerHTML += cardTag;
   });
 }
+let page = Math.floor(Math.random() * 10 + 1);
 fetch(
-  "https://api.pexels.com/v1/search?query=cyberpunk&per_page=18&orientation=landscape&size=small",
+  `https://api.pexels.com/v1/search?query=cyberpunk&per_page=18&orientation=landscape&size=small&page=${page}`,
   {
     headers: {
       Authorization: "563492ad6f91700001000001cf063f04a3ee4a43ba2bd2ba1fc03aa1",
